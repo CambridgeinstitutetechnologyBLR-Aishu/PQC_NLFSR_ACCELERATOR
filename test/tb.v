487f946ad44a25126f4c40bc77402693c73e889d
CambridgeinstitutetechnologyBLR-Aishu <aishwarya.24ece@cambridge.edu.in>
2026-05-30 22:01:17 +0530

    tb.v

--- test/tb.v
+++ test/tb.v
@@ -2,22 +2,34 @@
 `timescale 1ns / 1ps
 
 module tb ();
-  // ... (keep the top of the file the same)
+  initial begin
+    $dumpfile ("tb.vcd");
+    $dumpvars (0, tb);
+    #1;
+  end
 
-  // Replace the instantiation around line 31 with this:
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+
   tt_um_nlfsr_pqc user_project (
 `ifdef GL_TEST
       .VPWR(1'b1),
       .VGND(1'b0),
 `endif
-      .ui_in  (ui_in),    
-      .uo_out (uo_out),   
-      .uio_in (uio_in),   
-      .uio_out(uio_out),  
-      .uio_oe (uio_oe),   
-      .ena    (ena),      
-      .clk    (clk),      
-      .rst_n  (rst_n)     
+      .ui_in  (ui_in),
+      .uo_out (uo_out),
+      .uio_in (uio_in),
+      .uio_out(uio_out),
+      .uio_oe (uio_oe),
+      .ena    (ena),
+      .clk    (clk),
+      .rst_n  (rst_n)
   );
 
 endmodule
