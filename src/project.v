30ab402c7ecded75ab8770a012000a89fbbf1810
CambridgeinstitutetechnologyBLR-Aishu <aishwarya.24ece@cambridge.edu.in>
2026-05-30 21:28:33 +0530

    project.v

--- src/project.v
+++ src/project.v
@@ -1,25 +1,30 @@
 `default_nettype none
 
 module tt_um_nlfsr_pqc (
-    input  wire [7:0] ui_in,
-    output wire [7:0] uo_out,
-    input  wire [7:0] uio_in,
-    output wire [7:0] uio_out,
-    output wire [7:0] uio_oe,
-    input  wire       ena,
-    input  wire       clk,
-    input  wire       rst_n
+    input  wire [7:0] ui_in,    
+    output wire [7:0] uo_out,   
+    input  wire [7:0] uio_in,   
+    output wire [7:0] uio_out,  
+    output wire [7:0] uio_oe,   
+    input  wire       ena,      
+    input  wire       clk,      
+    input  wire       rst_n     
 );
 
     reg [7:0] lfsr;
     wire feedback = lfsr[7] ^ lfsr[5] ^ lfsr[4] ^ (lfsr[1] & lfsr[0]);
 
-    always @(posedge clk) begin
+    // Internal Clock Buffer to stop "Outside Die Area" errors
+    wire internal_clk = clk;
+
+    always @(posedge internal_clk) begin
         if (!rst_n) begin
-            lfsr <= 8'h01;
+            lfsr <= 8'h01; 
         end else if (ena) begin
-            if (uio_in[0]) lfsr <= (ui_in == 8'h00) ? 8'h01 : ui_in;
-            else if (uio_in[1]) lfsr <= {lfsr[6:0], feedback};
+            if (uio_in[0])
+                lfsr <= (ui_in == 8'h00) ? 8'h01 : ui_in;
+            else if (uio_in[1])
+                lfsr <= {lfsr[6:0], feedback};
         end
     end
 
